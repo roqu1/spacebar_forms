291ae164c889bd0dfe6206f0567d94f57eb91f72
Corneliu Rusu <snipklas@gmail.com>
2023-09-29 14:02:54 +0200

    AutoComplete with two words

---
+++ public/js/algolia-autocomplete.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".js-user-autocomplete").autocomplete({ hint: false }, [
+    {
+      source: function (query, cb) {
+        cb([{ value: "foo" }, { value: "bar" }]);
+      },
+    },
+  ]);
+});
