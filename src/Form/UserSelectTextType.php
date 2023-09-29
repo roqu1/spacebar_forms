aa900fb61cab6b553663b671b19ffdc919513f47
Corneliu Rusu <snipklas@gmail.com>
2023-09-29 17:26:25 +0200

    Autocomplete that shows up when you are searching for an email(by edit/create article)

--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -9,13 +9,16 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class UserSelectTextType extends AbstractType
 {
     private $userRepository;
+    private $router;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
     {
+        $this->router = $router;
         $this->userRepository = $userRepository;
     }
 
@@ -38,7 +41,8 @@ class UserSelectTextType extends AbstractType
                 return $userRepository->findOneBy(['email' => $email]);
             },
             'attr' => [
-                'class' => 'js-user-autocomplete'
+                'class' => 'js-user-autocomplete',
+                'data-autocomplete-url' => $this->router->generate('admin_utility_users')
             ]
         ]);
     }
