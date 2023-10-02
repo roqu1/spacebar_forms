39052f9d577b0d8dacefee6c806012b5fa3257b4
Corneliu Rusu <snipklas@gmail.com>
2023-10-02 17:15:26 +0200

    Added two fields with choices

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -35,6 +36,20 @@ class ArticleFormType extends AbstractType
 
             ->add("author", UserSelectTextType::class, [
                 'disabled' => $isEdit
+            ])
+            ->add("location", ChoiceType::class, [
+                'placeholder' => 'Choose a location',
+                'choices' => [
+                    'The Solar System' => 'solar_system',
+                    'Near a star' => 'star',
+                    'Interstellar Space' => 'interstellar_space'
+                ], 'required' => false,
+            ])
+            ->add("specificLocationName", ChoiceType::class, [
+                'placeholder' => 'Choose a location',
+                'choices' => [
+                    'TODO'
+                ], 'required' => false,
             ]);
 
         if ($options['include_published_at']) {
