04df261f2d0be36b70627ff0ccf445c50563655d
Corneliu Rusu <snipklas@gmail.com>
2023-10-04 12:14:22 +0200

    Adding/Deleting the second Location depending if first location has been set

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
@@ -60,6 +63,18 @@ class ArticleFormType extends AbstractType
                 'widget' => 'single_text'
             ]);
         }
+
+        $builder->get('location')->addEventListener(
+            FormEvents::POST_SUBMIT, // This event is triggered after the form is submitted
+            function (FormEvent $event) {
+                $form = $event->getForm(); // 
+                $this->setupSpecificLocationNameField(
+                    $form->getParent(),
+                    $form->getData()
+                ); // This method will add the specificLocationName field to the form
+
+            }
+        );
     }
 
     // This method suggests to Symfony what type of object this form is going to be used for
@@ -99,5 +114,26 @@ class ArticleFormType extends AbstractType
             'interstellar_space' => null,
         ];
         return $locationNameChoices[$location];
+    }
+
+    private function setupSpecificLocationNameField(FormInterface $form, ?string $location)
+    {
+        if (null === $location) {
+            $form->remove('specificLocationName');
+            return;
+        }
+
+        $choices = $this->getLocationNameChoices($location);
+
+        if (null === $choices) {
+            $form->remove('specificLocationName');
+            return;
+        }
+
+        $form->add("specificLocationName", ChoiceType::class, [
+            'placeholder' => 'Choose a location',
+            'choices' => $choices,
+            'required' => false,
+        ]);
     }
 }
